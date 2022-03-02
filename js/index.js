c6706261fb86d44b012425827e08abdbea1f160e
jakiasultana2022 <rokeyasbd@gmail.com>
2022-03-02 07:22:21 +0600

    search-field

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const searchPhone = () =>{
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    searchField.value = '';
+
+
+
+
+
+
+
+
+
+
+
+}
