39f3ce6b05db747d4513adf3a43667f69e2a407c
jakiasultana2022 <rokeyasbd@gmail.com>
2022-03-02 21:16:42 +0600

    search-result

--- js/index.js
+++ js/index.js
@@ -4,6 +4,37 @@ const searchPhone = () =>{
     console.log(searchText);
     searchField.value = '';
 
+    const url = `
+    https://openapi.programming-hero.com/api/phones?search=${searchText} `;
+   // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySearchResult (data.data.slice(0,20)));
+
+    // phone search result
+const displaySearchResult = data =>{
+    console.log(data);
+    const SearchResult= document.getElementById('search-result');
+    data.forEach (brand => {
+      console.log (brand) ;
+      const div =document.createElement('div');
+      div.classList.add('col');
+      div.innerHTML= `
+      <div onclick="" class="card h-100">
+      <img class="card-img-top" src="${brand.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+      <h5 class="card-title">${brand.brand}</h5>
+      <h5 class="card-title">${brand.phone_name}</h5>
+      
+      </div>
+    </div>
+       `;
+       SearchResult.appendChild(div);
+
+    })
+
+}
+
 
 
 
